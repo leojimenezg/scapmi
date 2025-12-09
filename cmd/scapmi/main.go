fedbd1f81b0067511073e4c811b5cbddba1f9662
LeoJimenezG <leonardojimenezgz@gmail.com>
2025-12-08 20:13:10 -0600

    feat: set OS hook to listen for specific events representing the program shortcuts

--- cmd/scapmi/main.go
+++ cmd/scapmi/main.go
@@ -2,9 +2,56 @@ package main
 
 import (
 	"fmt"
-	// "golang.design/x/clipboard"
+	"github.com/robotn/gohook"
+	"golang.design/x/clipboard"
+	"log"
+	"os"
 )
 
+var errStr string = "ERROR >> %s"
+var msgStr string = "MESSAGE >> %s"
+
+func init() {
+	err := clipboard.Init()
+	if err != nil {
+		msg := fmt.Sprintf(errStr, "your clipboard can't be used by scapmi :(")
+		log.Panic(msg)
+	}
+}
+
 func main() {
-	fmt.Println("scapmi >> Hello, World")
+	setHook()
+}
+
+func setHook() {
+	msg := fmt.Sprintf(msgStr, `press "Ctrl+Alt+q" any time to stop scapmi`)
+	log.Println(msg)
+
+	hook.Register(hook.KeyDown, []string{"ctrl", "alt", "q"}, func(e hook.Event) {
+		msg := fmt.Sprintf(msgStr, "goodbye for now!")
+		log.Println(msg)
+		hook.End()
+		os.Exit(0)
+	})
+
+	hook.Register(hook.KeyDown, []string{"ctrl", "alt", "c"}, func(e hook.Event) {
+		msg := fmt.Sprintf(msgStr, "copy shortcut pressed")
+		log.Println(msg)
+		// TODO: show interactive GUI to show and select slots in order to save content
+		// TODO: handle copied content in slots
+	})
+
+	hook.Register(hook.KeyDown, []string{"ctrl", "alt", "v"}, func(e hook.Event) {
+		msg := fmt.Sprintf(msgStr, "paste shortcut pressed")
+		log.Println(msg)
+		// TODO: show interactive GUI to show and select slots in order to paste content
+		// TODO: handle slots content and paste it without more user intervention
+	})
+
+	s := hook.Start()
+	<-hook.Process(s)
+}
+
+func showGUI() {
+
 }
--- go.mod
+++ go.mod
@@ -2,9 +2,13 @@ module github.com/leojimenezg/scapmi
 
 go 1.25.3
 
-require golang.design/x/clipboard v0.7.1
+require (
+	github.com/robotn/gohook v0.42.3
+	golang.design/x/clipboard v0.7.1
+)
 
 require (
+	github.com/vcaesar/keycode v0.10.1 // indirect
 	golang.org/x/exp/shiny v0.0.0-20250606033433-dcc06ee1d476 // indirect
 	golang.org/x/image v0.28.0 // indirect
 	golang.org/x/mobile v0.0.0-20250606033058-a2a15c67f36f // indirect
