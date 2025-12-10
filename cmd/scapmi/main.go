959b276de433a971663b8652c97109190942c232
LeoJimenezG <leonardojimenezgz@gmail.com>
2025-12-10 17:23:20 -0600

    feat: update logic of the entire application in order to be a desktop app and stablish core structure

--- cmd/scapmi/main.go
+++ cmd/scapmi/main.go
@@ -1,57 +1,128 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"github.com/robotn/gohook"
-	"golang.design/x/clipboard"
+	"gioui.org/app"
+	"gioui.org/font/gofont"
 	"log"
 	"os"
+	// "gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/text"
+	"gioui.org/widget/material"
+	"github.com/robotn/gohook"
+	"golang.design/x/clipboard"
 )
 
-var errStr string = "ERROR >> %s"
-var msgStr string = "MESSAGE >> %s"
+// ============================================================
+// Constants
+// ============================================================
+
+const (
+	StateInit AppState = iota
+	StateIdle
+	StateCopying
+	StatePasting
+)
+
+const errStr string = "ERROR >> %s"
+
+// ============================================================
+// Named Types
+// ============================================================
+
+type AppState int
+
+type Slot struct {
+	Content    []byte
+	HasContent bool
+}
+
+// ============================================================
+// Variables
+// ============================================================
+
+var Window *app.Window
+
+var CurrentAppState AppState
+
+var Slots [5]Slot
 
 func init() {
 	err := clipboard.Init()
 	if err != nil {
 		msg := fmt.Sprintf(errStr, "your clipboard can't be used by scapmi :(")
-		log.Panic(msg)
+		log.Fatal(msg)
 	}
+	Window = new(app.Window)
+	if Window == nil {
+		msg := fmt.Sprintf(errStr, "could not allocate memory for the window program")
+		log.Fatal(msg)
+	}
+	Window.Option(app.Title("scapmi"))
+	CurrentAppState = StateInit
 }
 
 func main() {
-	setHook()
+	go ShowGUI()
+	go SetHook()
+	go WatchClipboard()
+	app.Main()
 }
 
-func setHook() {
-	msg := fmt.Sprintf(msgStr, `press "Ctrl+Alt+q" any time to stop scapmi`)
-	log.Println(msg)
+func ShowGUI() {
+	th := material.NewTheme()
+	th.Shaper = text.NewShaper(text.WithCollection(gofont.Collection()))
+	for {
+		switch e := Window.Event().(type) {
+		case app.DestroyEvent:
+			os.Exit(0)
+		case app.FrameEvent:
+			var ops op.Ops
+			gtx := app.NewContext(&ops, e)
+			switch CurrentAppState {
+			case StateInit:
+				material.H1(th, "Welcome").Layout(gtx)
+			case StateIdle:
+				material.H1(th, "Idle State").Layout(gtx)
+			case StateCopying:
+				material.H1(th, "Copying State").Layout(gtx)
+				// TODO: Show buttons to select a slot and their state
+			case StatePasting:
+				material.H1(th, "Pasting State").Layout(gtx)
+				// TODO: Show slots with content and buttons to select them
+			}
+			e.Frame(gtx.Ops)
+		}
+	}
+}
 
+func SetHook() {
 	hook.Register(hook.KeyDown, []string{"ctrl", "alt", "q"}, func(e hook.Event) {
-		msg := fmt.Sprintf(msgStr, "goodbye for now!")
-		log.Println(msg)
 		hook.End()
 		os.Exit(0)
 	})
-
-	hook.Register(hook.KeyDown, []string{"ctrl", "alt", "c"}, func(e hook.Event) {
-		msg := fmt.Sprintf(msgStr, "copy shortcut pressed")
-		log.Println(msg)
-		// TODO: show interactive GUI to show and select slots in order to save content
-		// TODO: handle copied content in slots
-	})
-
 	hook.Register(hook.KeyDown, []string{"ctrl", "alt", "v"}, func(e hook.Event) {
-		msg := fmt.Sprintf(msgStr, "paste shortcut pressed")
-		log.Println(msg)
-		// TODO: show interactive GUI to show and select slots in order to paste content
-		// TODO: handle slots content and paste it without more user intervention
+		CurrentAppState = StatePasting
+		Window.Invalidate()
 	})
-
 	s := hook.Start()
 	<-hook.Process(s)
 }
 
-func showGUI() {
-
+func WatchClipboard() {
+	ctx := context.Background()
+	for {
+		txt := clipboard.Watch(ctx, clipboard.FmtText)
+		img := clipboard.Watch(ctx, clipboard.FmtImage)
+		select {
+		case <-txt:
+			CurrentAppState = StateCopying
+			Window.Invalidate()
+		case <-img:
+			CurrentAppState = StateCopying
+			Window.Invalidate()
+		}
+	}
 }
