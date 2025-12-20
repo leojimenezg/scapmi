e4692897fe705a9ad0dcca1e2c46038cc66eef4f
LeoJimenezG <leonardojimenezgz@gmail.com>
2025-12-19 19:47:24 -0600

    feat: add link widget, logic to open url still missing

--- gui/widgets/link.go
+++ gui/widgets/link.go
@@ -1,7 +1,30 @@
 package widgets
 
-import "gioui.org/layout"
+import (
+	"log"
 
-func TextLink(gtx layout.Context, text, url string) layout.Dimensions {
-	return layout.Dimensions{}
+	"gioui.org/io/pointer"
+	"gioui.org/layout"
+	"gioui.org/widget"
+	"gioui.org/widget/material"
+)
+
+type Link struct {
+	Button *widget.Clickable
+	Theme  *material.Theme
+	Text   string
+	Url    string
+}
+
+func NewLink(gtx layout.Context, obj Link) layout.Dimensions {
+	if obj.Button.Hovered() {
+		pointer.Cursor(pointer.CursorPointer).Add(gtx.Ops)
+	}
+	if obj.Button.Clicked(gtx) {
+		// TODO: Implement logic to open browser and go to the url
+		log.Printf("%s link was clicked", obj.Text)
+	}
+	return obj.Button.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+		return NewSmallText(gtx, obj.Theme, obj.Text)
+	})
 }
